0765c9812656d2838767c744ab857b222407002b
oykos-development-hub <hub@oykos.me>
2024-02-01 12:51:45 +0000

    Update code from GitLab

--- migrations/1706692816679598_non_financial_budgets.postgres.up.sql
+++ migrations/1706692816679598_non_financial_budgets.postgres.up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS non_financial_budgets (
     id serial PRIMARY KEY,
-    budget_id INTEGER NOT NULL REFERENCES budgets(id),
+    budget_id INTEGER NOT NULL REFERENCES budgets(id) ON DELETE CASCADE,
     organization_unit_id INTEGER NOT NULL,
     impl_contact_fullname TEXT NOT NULL,
     impl_contact_working_place TEXT NOT NULL,
