bf52282bf335802a65a970856d2802cd249c8711
oykos-development-hub <hub@oykos.me>
2024-04-25 08:10:23 +0000

    Update code from GitLab

--- migrations/1713969017644169_models_of_accountings.postgres.up.sql
+++ migrations/1713969017644169_models_of_accountings.postgres.up.sql
@@ -1,6 +1,7 @@
-CREATE TABLE IF NOT EXISTS models_of_accountings (
+CREATE TABLE IF NOT EXISTS models_of_accounting (
     id serial PRIMARY KEY,
     title VARCHAR ( 255 ) NOT NULL,
+    type TEXT,
     created_at TIMESTAMP,
     updated_at TIMESTAMP
 );
--- migrations/1713969094938804_model_of_accounting_items.postgres.up.sql
+++ migrations/1713969094938804_model_of_accounting_items.postgres.up.sql
@@ -7,3 +7,24 @@ CREATE TABLE IF NOT EXISTS model_of_accounting_items (
     created_at TIMESTAMP,
     updated_at TIMESTAMP
 );
+
+
+INSERT INTO models_of_accounting(title, type, created_at, updated_at) VALUES
+('Model knjiženja računa', 'invoices', NOW(), NOW()),
+('Model knjiženja rješenja', 'decisions', NOW(), NOW()),
+('Model knjiženja ugovora', 'contracts', NOW(), NOW()),
+('Model knjiženja zarada', 'salaries', NOW(), NOW());
+
+INSERT INTO model_of_accounting_items(title, model_id, debit_account_id, credit_account_id, created_at, updated_at) VALUES
+('Korektivni račun', 1, 1175, null, NOW(), NOW()),
+('Dobavljač', 1, null, 1175, NOW(), NOW()),
+('Korektivni račun', 2, 1175, null, NOW(), NOW()),
+('Dobavljač', 2, null, 1175, NOW(), NOW()),
+('Porez', 2, null, 1175, NOW(), NOW()),
+('Prirez', 2, null, 1175, NOW(), NOW()),
+('Korektivni račun', 3, 1175, null, NOW(), NOW()),
+('Dobavljač', 3, null, 1175, NOW(), NOW()),
+('Porez', 3, null, 1175, NOW(), NOW()),
+('Prirez', 3, null, 1175, NOW(), NOW());
+
+
