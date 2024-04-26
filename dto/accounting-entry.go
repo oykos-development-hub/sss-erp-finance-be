6452003bf37ab97448069e8928c63a92f0b3b222
oykos-development-hub <hub@oykos.me>
2024-04-26 07:30:46 +0000

    Update code from GitLab

--- dto/accounting-entry.go
+++ dto/accounting-entry.go
@@ -34,9 +34,10 @@ type ObligationForAccounting struct {
 }
 
 type AccountingOrderForObligationsData struct {
-	InvoiceID     []int     `json:"invoice_id"`
-	SalaryID      []int     `json:"salary_id"`
-	DateOfBooking time.Time `json:"date_of_booking"`
+	InvoiceID          []int     `json:"invoice_id"`
+	SalaryID           []int     `json:"salary_id"`
+	DateOfBooking      time.Time `json:"date_of_booking"`
+	OrganizationUnitID int       `json:"organization_unit_id"`
 }
 
 type AccountingOrderForObligations struct {
