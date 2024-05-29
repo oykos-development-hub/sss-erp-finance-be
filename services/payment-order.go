37b84503f107ec0606d69873927a5dbd6edca9cf
oykos-development-hub <hub@oykos.me>
2024-05-29 11:50:15 +0000

    Update code from GitLab

--- services/payment-order.go
+++ services/payment-order.go
@@ -318,6 +318,7 @@ func (h *PaymentOrderServiceImpl) GetPaymentOrderList(filter dto.PaymentOrderFil
 
 	if filter.Status == nil || *filter.Status != "Storniran" {
 		conditionAndExp = up.And(conditionAndExp, &up.Cond{"status <>": "Storniran"})
+		conditionAndExp = up.And(conditionAndExp, &up.Cond{"status is": nil})
 	}
 
 	//if filter.SortByTitle != nil {
